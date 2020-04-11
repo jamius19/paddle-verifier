90dc6578ad5be10f9c206939d9939b10b55138bc
Jamius Siam <jamiussiam@gmail.com>
2020-04-11 22:00:03 +0600

    Added bouncycastle dependency.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.bouncycastle:bcprov-jdk15on:1.65")
+
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
